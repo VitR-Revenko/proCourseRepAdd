3be5302e5b4d11f5cf64b15fb186d29189f0e4bd
VitR-Revenko <re.vitalii002@gmail.com>
2023-03-16 23:41:00 +0200

    hw17

--- src/js/script.js
+++ src/js/script.js
@@ -1,55 +1,44 @@
 "use strict";
 
-const images = Array.from(document.getElementsByClassName("js-img"));
-const buttonPrev = document.querySelector(".js-btn-prev");
-const buttonNext = document.querySelector(".js-btn-next");
-
-const hideClass = "hide";
-const showClass = "show";
-
-function removeShowClass() {
-  images.forEach((image) => image.classList.remove(showClass));
-}
-
-function hideButtons() {
-  const showItem = document.querySelector(".js-img.show");
-  const currentIndex = images.indexOf(showItem);
-  if (currentIndex + 1 === images.length) {
-    buttonNext.classList.add(hideClass);
-  } else {
-    buttonNext.classList.remove(hideClass);
-  }
-  if (currentIndex === 0) {
-    buttonPrev.classList.add(hideClass);
-  } else {
-    buttonPrev.classList.remove(hideClass);
-  }
-}
+const images = [
+  "img/img1.jpg",
+  "img/img2.png",
+  "img/img3.jpg",
+  "img/img4.jpg",
+  "img/img5.jpg",
+  "img/img6.jpg",
+  "img/img7.jpg",
+];
+const buttonPrev = document.getElementById("prev");
+const buttonNext = document.getElementById("next");
+const sliderContainer = document.getElementById("slider-container");
+let currentIndex = 0;
+let sliderInterval;
 
 buttonNext.addEventListener("click", () => {
-  if (buttonNext.classList.contains(hideClass)) {
-    return;
+  currentIndex++;
+  if (currentIndex >= images.length) {
+    currentIndex = 0;
   }
-  const showItem = document.querySelector(".js-img.show");
-  const currentIndex = images.indexOf(showItem);
-  removeShowClass();
-  buttonNext.classList.remove(hideClass);
-  images[currentIndex + 1].classList.add(showClass);
-  images[currentIndex + 1].classList.remove(hideClass);
-  images[currentIndex].classList.add(hideClass);
-  hideButtons();
+  sliderContainer.src = images[currentIndex];
+  clearInterval(setInterval(switchImage, 3000));
 });
 
 buttonPrev.addEventListener("click", () => {
-  if (buttonPrev.classList.contains(hideClass)) {
-    return;
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = images.length - 1;
   }
-  const showItem = document.querySelector(".js-img.show");
-  const currentIndex = images.indexOf(showItem);
-  removeShowClass();
-  buttonNext.classList.remove(hideClass);
-  images[currentIndex - 1].classList.add(showClass);
-  images[currentIndex - 1].classList.remove(hideClass);
-  images[currentIndex].classList.add(hideClass);
-  hideButtons();
+  sliderContainer.src = images[currentIndex];
+  clearInterval(setInterval(switchImage, 3000));
 });
+
+function switchImage() {
+  currentIndex++;
+  if (currentIndex >= images.length) {
+    currentIndex = 0;
+  }
+  sliderContainer.src = images[currentIndex];
+}
+
+setInterval(switchImage, 3000);
